a86c14c26356c32d55f82d0237ee8d1baf3be650
Nick Liu <nl2523@columbia.edu>
2016-04-23 15:15:36 -0400

    Running framework

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 var config = require('./config/config')
+var io = require('socket.io')(server);
 
 server.listen(config.port);
 app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public/static')));
 
-require('./routes/routes')(app)
+require('./routes/routes')(app, io, server);
 
 console.log("*****************************");
 console.log("* App running at port: " + config.port + " *");
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,33 @@
-module.exports = function (app){
+var path = require('path');
+//var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+
+module.exports = function (app, io, server){
+
+	//var http = require('http').Server(app);
+
 	app.get('/', function (req, res) {
-		res.render('index', {title : "Hello World"})
-	})
+		res.sendFile(path.join(__dirname , '../public/views/index.html'));
+	});
+
+	io.on('connection', function(socket) {
+		console.log('A user has connected');
+		
+		socket.on('chat message', function(msg){
+    		console.log('message: ' + msg);
+    		io.emit('chat message', msg);
+  		});
+
+
+
+		socket.on('disconnect', function() {
+			console.log('A user has disconnected');
+		});
+
+	});
+
+	server.listen(3000, function(){
+		console.log('Listening on *:3000');
+	});
+
 }
